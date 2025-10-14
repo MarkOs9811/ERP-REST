b1d1ed9c9c610a28e4c4445e12e506b8f0121d61
Marcos <marcosparitorres@gmail.com>
2025-10-14 16:52:26 -0500

    cambiando el icono de cargando

--- src/components/componentesReutilizables/Cargando.js
+++ src/components/componentesReutilizables/Cargando.js
@@ -1,11 +1,9 @@
+import { ChartGantt } from "lucide-react";
+
 export function Cargando() {
   return (
     <div style={{ textAlign: "center", padding: "20px" }}>
-      <img
-        src="/utilitarios/cargando.gif" // Ruta del GIF
-        alt="Cargando..."
-        style={{ width: "50px", height: "auto", opacity:0.2}} // Ajustar tamaño
-      />
+      <ChartGantt />
     </div>
   );
 }
